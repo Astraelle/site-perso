dcd3b9c4cf547cc637ca41f0f94c12815e4594f0
Charlie <charlie.pierre@orange.fr>
2023-01-15 17:03:07 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Lien</a>
+                </li>
+                <li>
+                    <a href="#">Lien</a>
+                </li>
+                <li>
+                    <a href="#">Lien</a>
+                </li>
+                <li>
+                    <a href="#">Lien</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div>
+
+    </div>
+</body>
+</html>
